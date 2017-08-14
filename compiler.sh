bef3e1523e4ff05353a289a2e66894fc161b457a
Charith Mendis <charithm@mit.edu>
2017-08-14 16:16:28 -0400

    config file changed; system recog added

--- compiler.sh
+++ compiler.sh
@@ -30,12 +30,22 @@ mkdir -p $COMP_BUILD_DIR
 cd $COMP_BUILD_DIR
 #build the release version of the compiler
 if [ "$COMP_GENERATE" != "0" ]; then
+    DARWIN=`expr "$(uname -a)" : '.*\(Darwin\).*'`
+    LINUX=`expr "$(uname -a)" : '.*\(Darwin\).*'`
+    if [ "$DARWIN" == ""] && [ "$LINUX" == "" ]; then
+	echo "Unknown system cannot run cmake"
+    fi
     if [ "$COMP_VERSION" != "Debug" ] || [ "$COMP_VERSION" != "Release" ]; then
 	echo "warning build type not selected or wrong type; building release version"
 	COMP_VERSION=Release
 	sleep 10
     fi
-    cmake -DLLVM_TARGETS_TO_BUILD=X86 -DC_INCLUDE_DIRS="/Library/Developer/CommandLineTools/usr/bin/../../usr/include/c++/v1:/Library/Developer/CommandLineTools/usr/bin/../../usr/include:/usr/include" -DLLVM_ENABLE_LIBCXX=ON -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=$COMP_VERSION $COMP_SRC_DIR
+    if [ "$DARWIN" != "" ]; then
+	cmake -DLLVM_TARGETS_TO_BUILD=X86 -DC_INCLUDE_DIRS="/Library/Developer/CommandLineTools/usr/bin/../../usr/include/c++/v1:/Library/Developer/CommandLineTools/usr/bin/../../usr/include:/usr/include" -DLLVM_ENABLE_LIBCXX=ON -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=$COMP_VERSION $COMP_SRC_DIR
+    fi
+    if [ "$LINUX" != "" ]; then
+	cmake -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_EH=ON -DLLVM_ENABLE_RTTI=ON -DCMAKE_BUILD_TYPE=$COMP_VERSION $COMP_SRC_DIR
+    fi
 fi
 
 make -j 4 clang
